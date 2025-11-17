416d477ecaf4bcc0f7bfc8a16c4b10fb7d3a4c8f
Kedrovaya-shishka <Misha12082008@icloud.com>
2025-11-17 19:29:05 +0200

    asda

--- nftMa/src/Page_Home/Page_Home.jsx
+++ nftMa/src/Page_Home/Page_Home.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import classes1 from '../Page_Home/Page_Home.module.scss'
 import n from './assets/spacewolk.png'
 import r from './assets/Avatar.png'
+import roket from './assets/RocketLaunch.png'
 
 function Page_Home() {
   const [count, setCount] = useState(0)
@@ -9,9 +10,29 @@ function Page_Home() {
   return (
     <>
         <main>
-          <section>
-             <div>
-              
+          <section className={classes1.digit}>
+            <div className={classes1.poddigit}>
+             <div className={classes1.dis}>
+              <h1>Discover digital art & Collect NFTs</h1>
+              <p>NFT marketplace UI created with Anima for Figma. Collect, buy and sell art from more than 20k NFT artists.</p>
+              <button>
+                <img src={roket} alt="" />
+                Get Started
+              </button>
+                <div className={classes1.ul}>
+                  <div>
+                      <h4>240k+ </h4>
+                      <p>Total Sale</p>
+                  </div>
+                  <div>
+                    <h4>100k+</h4>
+                    <p>Auctions</p>
+                  </div>
+                  <div>
+                    <h4>240k+ </h4>
+                    <p>Artists</p>
+                  </div>
+                </div>
              </div>
              <div className={classes1.cart}>
               <img src={n} alt="" />
@@ -21,6 +42,12 @@ function Page_Home() {
                 <p>Animakid</p>
               </div>
              </div>
+             </div>
+          </section>
+          <section className={classes1.trend}>
+            <div className={classes1.podtrend}>
+
+            </div>
           </section>
         </main>
     </>
